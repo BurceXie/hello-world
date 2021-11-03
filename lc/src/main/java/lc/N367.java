274bdacecf1fc3d41a209b68441e150e037d86f6
BruceXie <terryxie86@outlook.com>
2021-11-04 00:26:49 +0800

    0026

---
+++ lc/src/main/java/lc/N367.java
@@ -0,0 +1,23 @@
+package alc;
+
+import java.util.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.*;
+
+/**
+ *
+ *
+ */
+public class N {
+
+	public static void main(String[] args) {
+		Solution s = new N().new Solution();
+
+		System.out.println(s);
+	}
+
+class Solution {
+}
+}
+
